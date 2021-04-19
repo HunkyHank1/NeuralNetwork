22ad14526758bae40ada36589d70c3fc1d086b76
Henry Johnson <46233024+HunkyHank1@users.noreply.github.com>
2021-04-18 20:08:25 -0600

    this is an unwritten training program. More info in the readme

---
+++ Trainer.java
@@ -0,0 +1,11 @@
+public class Trainer {
+    private int[] personalweightings = new int[0];
+    public Trainer(){
+
+    }
+    //weightings based on the likelyness of certain players entering the field
+    public int[] weightings(){
+        this.personalweightings = personalweightings;
+        return personalweightings;
+    }
+}
